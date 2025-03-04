dcd0ce2f3af86315dccccff8846bf16ebe4e63a1
J. Sebastian Paez <jspaezp@users.noreply.github.com>
2025-03-04 00:39:29 -0600

    Release v0.5.12 (#59)
    * Bump abatilo/actions-poetry from 2.1.6 to 2.3.0
    
    Bumps [abatilo/actions-poetry](https://github.com/abatilo/actions-poetry) from 2.1.6 to 2.3.0.
    - [Release notes](https://github.com/abatilo/actions-poetry/releases)
    - [Changelog](https://github.com/abatilo/actions-poetry/blob/master/.releaserc)
    - [Commits](https://github.com/abatilo/actions-poetry/compare/v2.1.6...v2.3.0)
    
    ---
    updated-dependencies:
    - dependency-name: abatilo/actions-poetry
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump mkdocstrings from 0.19.1 to 0.25.1
    
    Bumps [mkdocstrings](https://github.com/mkdocstrings/mkdocstrings) from 0.19.1 to 0.25.1.
    - [Release notes](https://github.com/mkdocstrings/mkdocstrings/releases)
    - [Changelog](https://github.com/mkdocstrings/mkdocstrings/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/mkdocstrings/mkdocstrings/compare/0.19.1...0.25.1)
    
    ---
    updated-dependencies:
    - dependency-name: mkdocstrings
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * (chore) lock update and update in CICD (#35)
    
    * (chore) lock update and update in CICD
    * (chore) updated preprocessor and added testing
    * (chore) updated pre commit hooks and versions
    * (chore) updated test to match boostrap distro, improved test error msg.
    * (chore) upgraded missing versions of checkout and python setup in cicd
    
    * Bump mdformat-frontmatter from 0.4.1 to 2.0.8 (#41)
    
    Bumps [mdformat-frontmatter](https://github.com/butler54/mdformat-frontmatter) from 0.4.1 to 2.0.8.
    - [Release notes](https://github.com/butler54/mdformat-frontmatter/releases)
    - [Changelog](https://github.com/butler54/mdformat-frontmatter/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/butler54/mdformat-frontmatter/compare/v0.4.1...v2.0.8)
    
    ---
    updated-dependencies:
    - dependency-name: mdformat-frontmatter
      dependency-type: direct:development
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * fix: add support for arbitrary separator repetitions (#58)
    
    * fix: add support for arbitrary separator repetitions
    * chore: migrate to uv
    * chore: updated pre commit hooks to remove poetry
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- mkquartodocs/extension.py
+++ mkquartodocs/extension.py
@@ -14,12 +14,13 @@ log = get_logger(__name__)
 
 
 class AdmotionCellDataPreprocessor(Preprocessor):
-    CELL_REGEX: Final = re.compile(r"^::: \{\.cell .*}\s*$")
-    CELL_END: Final = re.compile(r"^:{3,4}?$")
+
+    CELL_REGEX: Final = re.compile(r"^(:{3,}) \{\.cell .*}\s*$")
+    CELL_END: Final = re.compile(r"^(:{3,})$")
     CELL_ELEM_REGEX: Final = re.compile(
-        r"^:{3,4} \{(\.cell(-\w+)?)\s?(\.cell-[\w-]+)?( execution_count=\"\d+\")?\}"
+        r"^(:{3,}) \{(.cell-\w+)\s?(\.cell-[\w-]+)?( execution_count=\"\d+\")?\}$"
     )
-    CODEBLOCK_REGEX: Final = re.compile(r"^```{\.(\w+) .*}")
+    CODEBLOCK_REGEX: Final = re.compile(r"^(`{3,}){\.(\w+) .*}")
 
     # https://squidfunk.github.io/mkdocs-material/reference/admonitions/#supported-types
     TYPE_MAPPING: Final = {
@@ -46,19 +47,17 @@ class AdmotionCellDataPreprocessor(Preprocessor):
             out = "\n\n"
 
         elif sr := self.CELL_ELEM_REGEX.search(line):
-            groups = {i: x for i, x in enumerate(sr.groups())}
-            log.debug(f"Matched Cell element: {line}, groups: {groups}")
-            if groups[3]:
-                out = "\n\n"
-            else:
-                output_type = groups[2]
-                out = self.TYPE_MAPPING[output_type]
+            grp = sr.groups()
+            log.debug(f"Matched Cell element: {line}, groups: {grp}")
+            output_type = grp[2]
+            out = self.TYPE_MAPPING[output_type]
 
         elif sr := self.CODEBLOCK_REGEX.search(line):
-            groups = {i: x for i, x in enumerate(sr.groups())}
-            log.debug(f"Matched codeblock: {line} -> {groups}")
-            lang = sr.groups(1)
-            out = f"```{lang}"
+            grp = sr.groups()
+            log.debug(f"Matched codeblock: {line}, groups: {grp}")
+            lang = grp[2]
+            nesting_level = len(grp[1])
+            out = f"{'`' * nesting_level}{lang}"
         else:
             out = line
 
