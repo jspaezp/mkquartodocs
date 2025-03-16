f9da89e4ddd41de9c4254e338e052b35f7d34697
J. Sebastian Paez <jspaezp@gmail.com>
2025-03-16 05:46:35 -0700

    wip: stateful preprocessor MVP with table

--- mkquartodocs/extension.py
+++ mkquartodocs/extension.py
@@ -8,7 +8,11 @@ from typing import Final
 from markdown import Markdown
 from markdown.extensions import Extension
 from markdown.extensions.admonition import AdmonitionProcessor
-from markdown.extensions.md_in_html import MarkdownInHtmlExtension
+from markdown.extensions.md_in_html import (
+    HtmlBlockPreprocessor,
+    MarkdownInHtmlProcessor,
+    MarkdownInHTMLPostprocessor,
+)
 from markdown.preprocessors import Preprocessor
 
 # Installed as pymdown-extensions
@@ -18,14 +22,20 @@ from .logging import get_logger
 
 log = get_logger(__name__)
 
+# `:::: {.cell execution_count="1"}`
+# `:::::: {.cell layout-align="default"}` <-  happens in mermaid diagrams
 CELL_REGEX: Final = re.compile(r"^(:{3,}) \{\.cell .*}\s*$")
 CELL_END: Final = re.compile(r"^(:{3,})$")
 
 # In theory it would be a 'cell-output' but there are other kinds of out
 # Cells contain multiple cell elements.
+# `::: {.cell-output .cell-output-stdout}`
 CELL_ELEM_REGEX: Final = re.compile(
     r"^(:{3,}) \{(.cell-\w+)\s?(\.cell-[\w-]+)?( execution_count=\"\d+\")?\}$"
 )
+# `::::: cell-output-display`
+CELL_ELEM_ALT_REGEX: Final = re.compile(r"^(:{3,})\s*(cell-output-display\s*)$")
+
 CODEBLOCK_REGEX: Final = re.compile(r"^(`{3,}){\.(\w+) .*}")
 
 # https://squidfunk.github.io/mkdocs-material/reference/admonitions/#supported-types
@@ -35,6 +45,7 @@ TYPE_MAPPING: Final = {
     ".cell-output-stderr": '???+ warning "stderr"',
     ".cell-output-error": '???+ danger "error"',
     ".cell-output-display": '???+ note "Display"',
+    "cell-output-display": "???+ note",
 }
 
 
@@ -78,6 +89,9 @@ class FileContent:
     def get_lines_content(self, start: Cursor, end: Cursor) -> list[str]:
         # TODO ... this is an off-by 1 error nightmare ...
         # Some unit testing might be really worth it ...
+        if start.line == end.line:
+            return [self._trim_line(self.lines[start.line], start=start, end=end)]
+
         first = self._trim_line(self.lines[start.line], start=start, end=None)
         last = self._trim_line(self.lines[end.line + 1], start=None, end=end)
         return [first] + self.lines[start.line + 1 : end.line] + [last]
@@ -153,6 +167,19 @@ class BlockContext:
         )
 
     @classmethod
+    def _from_cell_element_line_alt(cls, line: str, line_number: int) -> BlockContext:
+        """This function assumes that you already checked that the line matches CELL_ELEM_ALT_REGEX"""
+        sr = CELL_ELEM_ALT_REGEX.search(line)
+        grp = sr.groups()
+        return BlockContext(
+            block_type=BlockType.CELL_ELEM,
+            delimiter=grp[0],
+            attributes=grp[1:],
+            start=Cursor(line=line_number, col=0),
+            end=UnknownEnd(),
+        )
+
+    @classmethod
     def try_from_line(
         cls,
         file_content: FileContent,
@@ -168,6 +195,10 @@ class BlockContext:
             grp = sr.groups()
             log.debug(f"Matched Cell element: {line}, groups: {grp}")
             return BlockContext._from_cell_element_line(line, cursor.line)
+
+        elif sr := CELL_ELEM_ALT_REGEX.search(line):
+            log.debug(f"Matched Cell element alternative: {line}")
+            return BlockContext._from_cell_element_line_alt(line, cursor.line)
 
         elif sr := CODEBLOCK_REGEX.search(line):
             grp = sr.groups()
@@ -229,7 +260,7 @@ class BlockContext:
             line = file_content.get_line_content(local_cursor)
             if local_regex.match(line):
                 log.debug(f"Matched {self.block_type} end: {line}")
-                out = Cursor(local_cursor.line, len(line))
+                out = Cursor(local_cursor.line, len(line) + 1)
                 return out
             local_cursor = local_cursor.advance_line(1)
 
@@ -271,7 +302,7 @@ class BlockContext:
             ]
             show = "\n" + "\n".join(show)
             msg = f"BlockContext {self} contains starting :::, which is not allowed: {show}"
-            raise ValueError(msg)
+            raise RuntimeError(msg)
 
         return out
 
@@ -298,6 +329,7 @@ class BlockContext:
     def _into_output_lines_cell_elem(self, file_content: FileContent) -> list[str]:
         if isinstance(self.end, UnknownEnd):
             raise ValueError(f"BlockContext {self} has no end")
+
         out = []
         admon_candidates = [
             TYPE_MAPPING[k] for k in self.attributes if k in TYPE_MAPPING
@@ -312,17 +344,35 @@ class BlockContext:
 
         internal = []
         last_end = self.start.advance_line(1)
-        tmp = self.find_inner_block(file_content, context=[], start=last_end)
-        while tmp is not None:
-            inter = file_content.get_lines_content(last_end, tmp.start)
-            inner = tmp.into_output_lines(file_content)
-            internal.extend(inter)
-            internal.extend(inner)
-            last_end = tmp.end
+        # Do special handing of <div>
+        # This whole branch is kind of ugly tbh ..
+        # I should move it to its own "kind" of block
+        if "div" in file_content.get_line_content(last_end):
+            internal = file_content.get_lines_content(
+                last_end, self.end.advance_line(0)
+            )
+            # if the first line is a starting div tag and the last non-empty is a
+            # closing div tag, then we just add the
+            # markdown in html attribute and leave as is.
+            non_empty = [line for line in internal if line.strip()]
+            if internal[0].startswith("<div>") and non_empty[-1].endswith("</div>"):
+                if "style" in internal[1]:
+                    internal[0] = '<div markdown="block">'
+
+                last_end = self.end
+
+        else:
             tmp = self.find_inner_block(file_content, context=[], start=last_end)
+            while tmp is not None:
+                inter = file_content.get_lines_content(last_end, tmp.start)
+                inner = tmp.into_output_lines(file_content)
+                internal.extend(inter)
+                internal.extend(inner)
+                last_end = tmp.end
+                tmp = self.find_inner_block(file_content, context=[], start=last_end)
 
         internal.extend(
-            file_content.get_lines_content(last_end, self.end.advance_line(-1))
+            file_content.get_lines_content(last_end, self.end.advance_line(0))
         )
         out.extend([" " * 4 + line for line in internal])
         out.append("\n\n")
@@ -412,3 +462,13 @@ class QuartoCellDataExtension(Extension):
             "details",
             104,
         )
+
+        # essentially re-impement MarkdownInHtmlExtension
+        # Replace raw HTML preprocessor
+        md.preprocessors.register(HtmlBlockPreprocessor(md), "html_block", 20)
+        # Add `blockprocessor` which handles the placeholders for `etree` elements
+        md.parser.blockprocessors.register(
+            MarkdownInHtmlProcessor(md.parser), "markdown_block", 105
+        )
+        # Replace raw HTML postprocessor
+        md.postprocessors.register(MarkdownInHTMLPostprocessor(md), "raw_html", 30)
