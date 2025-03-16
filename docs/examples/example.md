4a7ff7df255b77fdb8123ee32cb4e0aba362e96b
J. Sebastian Paez <jspaezp@gmail.com>
2025-03-16 04:15:42 -0700

    wip: stateful preprocessor MVP

--- docs/examples/example.md
+++ docs/examples/example.md
@@ -1,4 +1,15 @@
 ---
+biblio-config: true
+labels:
+  abstract: Abstract
+  affiliations: Affiliations
+  authors: Authors
+  description: Description
+  doi: Doi
+  keywords: Keywords
+  modified: Modified
+  published: Published
+  related_formats: Other Formats
 title: This is a simple example that uses python
 toc-title: Table of contents
 ---
@@ -13,113 +24,70 @@ But we can use python to check the contents of this file!
 ``` {.python .cell-code}
 with open("example.qmd", "r") as f:
     for l in f:
-        print("> ", str(l))
+        print(str(l).strip())
 ```
 
 ::: {.cell-output .cell-output-stdout}
-    >
 
-    >  # This is a simple example that uses python
+    # This is a simple example that uses python
 
-    >
+    You can take a look at the .qmd that generated this docs
+    from:
 
-    >  You can take a look at the .qmd that generated this docs
+    [https://github.com/jspaezp/mkquartodocs/tree/main/docs](https://github.com/jspaezp/mkquartodocs/tree/main/docs)
 
-    >  from:
 
-    >
+    But we can use python to check the contents of this file!
 
-    >  [https://github.com/jspaezp/mkquartodocs/tree/main/docs](https://github.com/jspaezp/mkquartodocs/tree/main/docs)
+    ```{python}
+    with open("example.qmd", "r") as f:
+    for l in f:
+    print(str(l).strip())
 
-    >
+    ```
 
-    >
 
-    >  But we can use python to check the contents of this file!
+    # Hello
 
-    >
+    Here is some text
 
-    >  ```{python}
 
-    >  with open("example.qmd", "r") as f:
+    ```{python}
+    print("Hello World")
+    ```
 
-    >      for l in f:
+    Here is how warnings look
 
-    >          print("> ", str(l))
 
-    >
+    ```{python}
+    import warnings
+    warnings.warn("This is a warning")
+    ```
 
-    >  ```
+    Some python logic
 
-    >
 
-    >
+    ```{python}
+    # Fizz buzz as a simple list comprehension
+    out = ["Fizz"*(i%3==0)+"Buzz"*(i%5==0) or str(i) for i in range(1, 101)]
+    print(out)
+    ```
 
-    >  # Hello
 
-    >
+    Some pretty pritnting
 
-    >  Here is some text
+    ```{python}
+    from pprint import pprint
+    pprint(out)
+    ```
 
-    >
+    And finally an error just to see how it looks
 
-    >
 
-    >  ```{python}
-
-    >  print("Hello World")
-
-    >  ```
-
-    >
-
-    >  Here is how warnings look
-
-    >
-
-    >
-
-    >  ```{python}
-
-    >  import warnings
-
-    >  warnings.warn("This is a warning")
-
-    >  ```
-
-    >
-
-    >  Some python logic
-
-    >
-
-    >
-
-    >  ```{python}
-
-    >  # Fizz buzz as a simple list comprehension
-
-    >  out = ["Fizz"*(i%3==0)+"Buzz"*(i%5==0) or str(i) for i in range(1, 101)]
-
-    >  print(out)
-
-    >  ```
-
-    >
-
-    >  And finally an error just to see how it looks
-
-    >
-
-    >
-
-    >  ```{python}
-
-    >  #| error: true
-
-    >  raise NotImplementedError
-
-    >  ```
+    ```{python}
+    #| error: true
+    raise NotImplementedError
+    ```
 :::
 ::::
 
@@ -146,7 +114,7 @@ warnings.warn("This is a warning")
 ```
 
 ::: {.cell-output .cell-output-stderr}
-    /var/folders/42/tyvw22cj7t35kbjs0frhf_vw0000gp/T/ipykernel_26837/1338237052.py:2: UserWarning: This is a warning
+    /var/folders/42/tyvw22cj7t35kbjs0frhf_vw0000gp/T/ipykernel_9820/1338237052.py:2: UserWarning: This is a warning
       warnings.warn("This is a warning")
 :::
 ::::
@@ -165,20 +133,132 @@ print(out)
 :::
 ::::
 
-And finally an error just to see how it looks
+Some pretty pritnting
 
 :::: {.cell execution_count="5"}
+``` {.python .cell-code}
+from pprint import pprint
+pprint(out)
+```
+
+::: {.cell-output .cell-output-stdout}
+    ['1',
+     '2',
+     'Fizz',
+     '4',
+     'Buzz',
+     'Fizz',
+     '7',
+     '8',
+     'Fizz',
+     'Buzz',
+     '11',
+     'Fizz',
+     '13',
+     '14',
+     'FizzBuzz',
+     '16',
+     '17',
+     'Fizz',
+     '19',
+     'Buzz',
+     'Fizz',
+     '22',
+     '23',
+     'Fizz',
+     'Buzz',
+     '26',
+     'Fizz',
+     '28',
+     '29',
+     'FizzBuzz',
+     '31',
+     '32',
+     'Fizz',
+     '34',
+     'Buzz',
+     'Fizz',
+     '37',
+     '38',
+     'Fizz',
+     'Buzz',
+     '41',
+     'Fizz',
+     '43',
+     '44',
+     'FizzBuzz',
+     '46',
+     '47',
+     'Fizz',
+     '49',
+     'Buzz',
+     'Fizz',
+     '52',
+     '53',
+     'Fizz',
+     'Buzz',
+     '56',
+     'Fizz',
+     '58',
+     '59',
+     'FizzBuzz',
+     '61',
+     '62',
+     'Fizz',
+     '64',
+     'Buzz',
+     'Fizz',
+     '67',
+     '68',
+     'Fizz',
+     'Buzz',
+     '71',
+     'Fizz',
+     '73',
+     '74',
+     'FizzBuzz',
+     '76',
+     '77',
+     'Fizz',
+     '79',
+     'Buzz',
+     'Fizz',
+     '82',
+     '83',
+     'Fizz',
+     'Buzz',
+     '86',
+     'Fizz',
+     '88',
+     '89',
+     'FizzBuzz',
+     '91',
+     '92',
+     'Fizz',
+     '94',
+     'Buzz',
+     'Fizz',
+     '97',
+     '98',
+     'Fizz',
+     'Buzz']
+:::
+::::
+
+And finally an error just to see how it looks
+
+:::: {.cell execution_count="6"}
 ``` {.python .cell-code}
 raise NotImplementedError
 ```
 
 ::: {.cell-output .cell-output-error}
-    NotImplementedError:
+    NotImplementedError: 
     [0;31m---------------------------------------------------------------------------[0m
     [0;31mNotImplementedError[0m                       Traceback (most recent call last)
-    Cell [0;32mIn[5], line 1[0m
+    Cell [0;32mIn[24], line 1[0m
     [0;32m----> 1[0m [38;5;28;01mraise[39;00m [38;5;167;01mNotImplementedError[39;00m
 
-    [0;31mNotImplementedError[0m:
+    [0;31mNotImplementedError[0m: 
 :::
 ::::
--- mkquartodocs/plugin.py
+++ mkquartodocs/plugin.py
@@ -4,6 +4,7 @@ import shutil
 import subprocess
 import warnings
 from pathlib import Path
+from dataclasses import dataclass
 
 from mkdocs.config import config_options
 from mkdocs.plugins import BasePlugin
@@ -12,6 +13,7 @@ from mkquartodocs.extension import QuartoCellDataExtension
 
 from .context import DirWatcherContext
 from .logging import get_logger
+from .utils import convert_nav
 
 log = get_logger(__name__)
 
@@ -25,21 +27,90 @@ def _delete_file(path):
         path.rmdir()
 
 
+@dataclass
+class QuartoRuner:
+    quarto_path: Path
+
+    def run(self, *args, **kwargs):
+        subprocess.run([self.quarto_path, *args], **kwargs)
+
+    def run_with_retries(self, quarto_file, num_retries=5):
+        for i in range(num_retries):
+            try:
+                subprocess.run(
+                    [
+                        self.quarto_path,
+                        "render",
+                        str(quarto_file),
+                        "--to=markdown-simple_tables",
+                    ],
+                    check=True,
+                )
+                break
+            except subprocess.CalledProcessError as e:
+                # ERROR: Couldn't find open server
+                # it ocasionally fails with that error ...
+                if i >= num_retries:
+                    log.error(
+                        f"Quarto failed to render {quarto_file} after {num_retries} tries"
+                    )
+                    log.error(f"Quarto failed with error: {e}")
+                    raise
+                warnings.warn(f"Quarto failed to render {quarto_file}, retrying")
+                time.sleep(2)
+
+
+@dataclass
+class MkQuartodocsConfig:
+    quarto_path: Path
+    ignore_pattern: str | None = None
+    keep_output: bool = False
+    force_rebuild: bool = False
+
+    @classmethod
+    def from_mkdocs_config(cls, config):
+        config = config["plugins"]["mkquartodocs"].config
+        keep = ["quarto_path", "ignore_pattern", "keep_output", "force_rebuild"]
+        deprecated = {"ignore": "ignore_pattern"}
+        keep_dict = {k: config[k] for k in keep if k in config}
+        for k, v in deprecated.items():
+            if k in config:
+                log.warning(f"{k} is deprecated, use {v} instead")
+                keep_dict[v] = config[k]
+
+        # if extras:
+        #     raise ValueError(f"Unknown configuration options: {extras}")
+
+        if not keep_dict["quarto_path"]:
+            keep_dict["quarto_path"] = shutil.which("quarto")
+        else:
+            keep_dict["quarto_path"] = Path(keep_dict["quarto_path"])
+
+        log.debug(f"Configuration: {keep_dict}")
+
+        return cls(**keep_dict)
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+    def __setitem__(self, key, value):
+        setattr(self, key, value)
+
+
 class MkQuartoDocsPlugin(BasePlugin):
     config_scheme = (
         ("quarto_path", config_options.Type(Path)),
-        ("ignore", config_options.Type(str)),
+        ("ignore_pattern", config_options.Type(str)),
         ("keep_output", config_options.Type(bool, default=False)),
+        ("force_rebuild", config_options.Type(bool, default=False)),
     )
 
     def on_config(self, config, **kwargs):
-        passed_path = self.config["quarto_path"]
-        quarto = shutil.which(passed_path if passed_path else "quarto")
-        self.config["quarto_path"] = quarto
-        # self.ignores = [re.compile(x) for x in self.config["ignore"]]
+        self.config = MkQuartodocsConfig.from_mkdocs_config(config)
+        config["nav"] = convert_nav(config["nav"])
 
-        if self.config["ignore"]:
-            self.ignores = [re.compile(self.config["ignore"])]
+        if self.config["ignore_pattern"]:
+            self.ignores = [re.compile(self.config["ignore_pattern"])]
         else:
             self.ignores = []
         self.exit_action = _delete_file if not self.config["keep_output"] else None
@@ -56,51 +127,39 @@ class MkQuartoDocsPlugin(BasePlugin):
 
         return out
 
-    def on_pre_build(self, config):
-        quarto = self.config["quarto_path"]
-        docs_dir = config["docs_dir"]
-
+    def _get_quarto_docs(self, docs_dir):
         quarto_docs = Path(docs_dir).rglob("*.qmd")
         quarto_docs = [str(x) for x in quarto_docs]
         quarto_docs = self._filter_ignores(quarto_docs)
+        return quarto_docs
+
+    def on_pre_build(self, config):
+        quarto = self.config["quarto_path"]
+        docs_dir = config["docs_dir"]
 
         self.dir_context = DirWatcherContext(
             docs_dir, exit_action=self.exit_action, update_on_exit=False
         )
         self.dir_context.enter()
+        quarto_docs = self._get_quarto_docs(docs_dir)
+        runner = QuartoRuner(quarto)
         if quarto_docs:
             for x in quarto_docs:
                 x = Path(x)
                 parent_path = x.parent
                 expected_out = Path(parent_path) / (x.stem + ".md")
-                if expected_out.exists():
+                if expected_out.exists() and not self.config["force_rebuild"]:
                     qmd_mtime = x.stat().st_mtime
                     md_mtime = expected_out.stat().st_mtime
                     if qmd_mtime < md_mtime:
                         log.info(f"Skipping {x} as it is older than {expected_out}")
                         continue
                 log.info(f"Rendering {x}")
-                for i in range(5):
-                    try:
-                        subprocess.run(
-                            [quarto, "render", str(x), "--to=markdown"],
-                            check=True,
-                            # Note I use here markdown instead of gfm because
-                            # it explicitly renders mermaid diagrams
-                        )
-                        break
-                    except subprocess.CalledProcessError as e:
-                        # ERROR: Couldn't find open server
-                        # it ocasionally fails with that error ...
-                        if i >= 4:
-                            log.error(f"Quarto failed to render {x} after 5 tries")
-                            log.error(f"Quarto failed with error: {e}")
-                            raise
-                        warnings.warn(f"Quarto failed to render {x}, retrying")
-                        time.sleep(2)
+                runner.run_with_retries(x)
         else:
             warnings.warn(f"No quarto files were found in directory {docs_dir}")
 
+        log.info("Updating directory context")
         log.info(self.dir_context.update())
 
     def on_post_build(self, config):
